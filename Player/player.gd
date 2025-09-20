c6549a51d144137eb5daa8d2c30cd6b75782b460
John Doe <johndoe@gmail.com>
2025-09-19 22:41:05 -0600

    -> void

--- Player/player.gd
+++ Player/player.gd
@@ -7,12 +7,14 @@ var move_dir = Vector2()
 @export var knockback_multiplier = 1
 var knockback = Vector2()
 
-var disabled = false;
+var disabled = false
+
+var color_index = -1
 
 func _ready() -> void:
 	add_to_group("player")
 
-func _process(delta):
+func _process(delta) -> void:
 	var window_size = get_window().size  # Get current window size
 	var half_player_size = Vector2(10, 10)  # Adjust based on player sprite size/2
 
@@ -20,7 +22,7 @@ func _process(delta):
 	position.y = clamp(position.y, half_player_size.y, window_size.y - half_player_size.y)
 	pass
 
-func _physics_process(delta):
+func _physics_process(delta) -> void:
 	if disabled:
 		return
 
@@ -32,23 +34,23 @@ func _physics_process(delta):
 	
 	move_and_slide()
 
-func get_input():
+func get_input() -> void:
 	set_move_dir()
 
-func set_move_dir():
+func set_move_dir() -> void:
 	move_dir.x += int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
 	move_dir.y += int(Input.is_action_pressed("move_down")) - int(Input.is_action_pressed("move_up"))
 	move_dir = move_dir.normalized()
 
-func knockback_taken(area, knockback_amount):
+func knockback_taken(area, knockback_amount) -> void:
 	knockback = global_position.direction_to(area.global_position) * knockback_amount
 
-func swap_in():
+func swap_in() -> void:
 	disabled = false
 	visible = true
 	$Hurtbox/CollisionShape2D.disabled = false
 
-func swap_out():
+func swap_out() -> void:
 	disabled = true
 	visible = false
 	$Hurtbox/CollisionShape2D.disabled = true
